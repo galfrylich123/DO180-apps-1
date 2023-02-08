d50255939c0d0f1e915c007ac637d4f4324cd2e6
Almog Sofer <45693218+TakNud@users.noreply.github.com>
2023-02-08 14:59:02 +0200

    new Try From new branch

---
+++ AmirScript.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CurCommitHash=$(git log --pretty=format:'%h' -n 1 origin/master)
+BeforeCommitHash=$(git log --pretty=format:'%h' -n 1 origin/master^)
+Changes=$(git diff --name-only ${CurCommitHash} ${BeforeCommitHash})
+Folders=("hello.txt" "sampleFolder" "nodejs-app" "newThing")
+Flag='FALSE'
+
+
+echo "Current hash -> ${CurCommitHash}"
+echo "Before hash -> ${BeforeCommitHash}"
+echo "Changes folders -> ${Changes}"
+
+for F in ${Folders[@]}; do
+    for change in ${Changes[@]}; do
+        if [[ "$change" == *"$F"* ]]; then
+            echo "${change} exist in our files array"
+            Flag='TRUE'
+            #break
+        fi
+    done
+done
+
+if [[ "$Flag" = "FALSE" ]]; then
+    echo "ABORTTTTTT"
+fi
