4fc6d86eec848562229e4dd5fd221c38f915efba
Gal Frylich <galfrylich@Gal-Frylichs-MacBook-Pro.local>
2023-02-14 12:36:03 +0200

    master commit

--- newscript.sh
+++ newscript.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 CurCommitHash=$(git rev-parse HEAD)
-BeforeCommitHash=$(git merge-base HEAD $(git rev-list --parents HEAD | awk '{print $2}'))
-#BeforeMergeHash=$(git log --pretty=format:'%h' --merges origin/master^)
+BeforeCommitHash=$(git log --left-right --boundary --pretty=format:"%h" master^..master | tail -1)
 Folders=("todoapp" "temps" "nodejs-app" "newThing")
 Flag='FALSE'
 
-# if [[-z $BeforeMergeHash]]; then
-Changes=$(git diff --name-only ${CurCommitHash} ${BeforeCommitHash})
-# else
-#Changes=$(git diff --name-only ${CurCommitHash} ${BeforeMerg})
+
+ Changes=$(git diff --name-only ${CurCommitHash} ${BeforeCommitHash})
+
 
 echo "Current hash -> ${CurCommitHash}"
 echo "Before hash -> ${BeforeCommitHash}"
-echo "Changes folders -> $Changes"
+
 
 for F in ${Folders[@]}; do
     for change in ${Changes[@]}; do
         if [[ "$change" == *"$F"* ]]; then
             echo "${change} exist in our files array"
             Flag='TRUE'
-            #break
+            
         fi
     done
 done
@@ -28,3 +26,4 @@ done
 if [[ "$Flag" = "FALSE" ]]; then
     echo "ABORTTTTTT"
 fi
+
